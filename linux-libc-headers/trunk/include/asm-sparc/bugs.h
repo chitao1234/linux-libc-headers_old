a2e900ad41d148099fff0385565aa47725e4c609
mmazur <mmazur@11da6c6d-24c7-0310-97e0-b6d17e038e62>
2003-12-15 18:46:56 +0000

    Initial revision

---
+++ linux-libc-headers/trunk/include/asm-sparc/bugs.h
@@ -0,0 +1,7 @@
+/*  $Id: bugs.h,v 1.1 2003/12/15 18:47:00 mmazur Exp $
+ *  include/asm-sparc/bugs.h:  Sparc probes for various bugs.
+ *
+ *  Copyright (C) 1994 David S. Miller (davem@caip.rutgers.edu)
+ */
+
+static void check_bugs(void) { }
---
+++ linux-libc-headers/trunk/include/linux/mtd/concat.h
@@ -0,0 +1,23 @@
+/*
+ * MTD device concatenation layer definitions
+ *
+ * (C) 2002 Robert Kaiser <rkaiser@sysgo.de>
+ *
+ * This code is GPL
+ *
+ * $Id: concat.h,v 1.1 2003/12/15 18:46:58 mmazur Exp $
+ */
+
+#ifndef MTD_CONCAT_H
+#define MTD_CONCAT_H
+
+
+struct mtd_info *mtd_concat_create(
+    struct mtd_info *subdev[],  /* subdevices to concatenate */
+    int num_devs,               /* number of subdevices      */
+    char *name);                /* name for the new device   */
+
+void mtd_concat_destroy(struct mtd_info *mtd);
+
+#endif
+
