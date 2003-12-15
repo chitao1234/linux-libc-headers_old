4b30478c543b26d3536861d8e696e396c4e377f3
mmazur <mmazur@11da6c6d-24c7-0310-97e0-b6d17e038e62>
2003-12-15 18:46:56 +0000

    Initial revision

---
+++ include/asm-sparc/eeprom.h
@@ -0,0 +1,9 @@
+/* $Id: eeprom.h,v 1.1 2003/12/15 18:47:00 mmazur Exp $
+ * eeprom.h:  Definitions for the Sun eeprom.
+ *
+ * Copyright (C) 1995 David S. Miller (davem@caip.rutgers.edu)
+ */
+
+/* The EEPROM and the Mostek Mk48t02 use the same IO address space
+ * for their registers/data areas.  The IDPROM lives here too.
+ */
