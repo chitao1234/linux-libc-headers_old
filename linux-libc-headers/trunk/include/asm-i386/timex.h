8ce32aed39a0518eb6c2af7ce439294470c4ba3b
qboosh <qboosh@11da6c6d-24c7-0310-97e0-b6d17e038e62>
2003-12-23 21:07:31 +0000

    - userspace version

--- linux-libc-headers/trunk/include/asm-i386/timex.h
+++ linux-libc-headers/trunk/include/asm-i386/timex.h
@@ -6,16 +6,10 @@
 #ifndef _ASMi386_TIMEX_H
 #define _ASMi386_TIMEX_H
 
-#include <linux/config.h>
-
-#ifdef CONFIG_X86_PC9800
-   extern int CLOCK_TICK_RATE;
-#else
-#ifdef CONFIG_MELAN
+#if 0 && defined(CONFIG_MELAN)
 #  define CLOCK_TICK_RATE 1189200 /* AMD Elan has different frequency! */
 #else
 #  define CLOCK_TICK_RATE 1193182 /* Underlying HZ */
-#endif
 #endif
 
 #define CLOCK_TICK_FACTOR	20	/* Factor of both 1000000 and CLOCK_TICK_RATE */
@@ -38,21 +32,5 @@
  * regardless of how fast the machine is. 
  */
 typedef unsigned long long cycles_t;
-
-extern cycles_t cacheflush_time;
-
-static inline cycles_t get_cycles (void)
-{
-#ifndef CONFIG_X86_TSC
-	return 0;
-#else
-	unsigned long long ret;
-
-	rdtscll(ret);
-	return ret;
-#endif
-}
-
-extern unsigned long cpu_khz;
 
 #endif
