8260a92acf6e97047a6c2165bbdecd120c0a65fe
mmazur <mmazur@11da6c6d-24c7-0310-97e0-b6d17e038e62>
2004-02-08 16:42:23 +0000

    - releasing 2.6.1.3 - ARGHHHHH - the host where I work on llh died two hours ago... taking my   almost finished 2.6.2.0 with it :/

--- include/linux/version.h
+++ include/linux/version.h
@@ -4,7 +4,7 @@
 
 #define UTS_RELEASE "2.6.1"
 #define LINUX_VERSION_CODE 132608
-#define LIBC_HEADERS_VERSION "2.6.1.2"
+#define LIBC_HEADERS_VERSION "2.6.1.3"
 #define KERNEL_VERSION(a,b,c) (((a) << 16) + ((b) << 8) + (c))
 
 #endif
