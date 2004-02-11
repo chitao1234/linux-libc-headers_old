543f61474d88a18d5565392371218b240ec1d6c5
mmazur <mmazur@11da6c6d-24c7-0310-97e0-b6d17e038e62>
2004-02-11 18:44:04 +0000

    - glibc has io.h header for some archs. On these archs we include what glibc   gives us

--- include/asm-sh/io.h
+++ include/asm-sh/io.h
@@ -1,31 +1,3 @@
-#ifndef __ASM_SH_IO_H
-#define __ASM_SH_IO_H
+#warning "You should include <sys/io.h>. This time I will do it for you."
+#include <sys/io.h>
 
-/*
- * Convention:
- *    read{b,w,l}/write{b,w,l} are for PCI,
- *    while in{b,w,l}/out{b,w,l} are for ISA
- * These may (will) be platform specific function.
- * In addition we have 'pausing' versions: in{b,w,l}_p/out{b,w,l}_p
- * and 'string' versions: ins{b,w,l}/outs{b,w,l}
- * For read{b,w,l} and write{b,w,l} there are also __raw versions, which
- * do not have a memory barrier after them.
- *
- * In addition, we have 
- *   ctrl_in{b,w,l}/ctrl_out{b,w,l} for SuperH specific I/O.
- *   which are processor specific.
- */
-
-/*
- * We follow the Alpha convention here:
- *  __inb expands to an inline function call (which either calls via the
- *        mach_vec if generic, or a machine specific implementation)
- *  _inb  is a real function call (note ___raw fns are _ version of __raw)
- *  inb   by default expands to _inb, but the machine specific code may
- *        define it to __inb if it chooses.
- */
-
-#include <asm/cache.h>
-#include <asm/system.h>
-
-#endif /* __ASM_SH_IO_H */
