b83bff3a82acea31c79d582c1727e6397eb8a347
mmazur <mmazur@11da6c6d-24c7-0310-97e0-b6d17e038e62>
2004-02-11 18:44:04 +0000

    - glibc has io.h header for some archs. On these archs we include what glibc   gives us

--- linux-libc-headers/trunk/include/asm-arm/io.h
+++ linux-libc-headers/trunk/include/asm-arm/io.h
@@ -1,23 +1,3 @@
-/*
- *  linux/include/asm-arm/io.h
- *
- *  Copyright (C) 1996-2000 Russell King
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2 as
- * published by the Free Software Foundation.
- *
- * Modifications:
- *  16-Sep-1996	RMK	Inlined the inx/outx functions & optimised for both
- *			constant addresses and variable addresses.
- *  04-Dec-1997	RMK	Moved a lot of this stuff to the new architecture
- *			specific IO header files.
- *  27-Mar-1999	PJB	Second parameter of memcpy_toio is const..
- *  04-Apr-1999	PJB	Added check_signature.
- *  12-Dec-1999	RMK	More cleanups
- *  18-Jun-2000 RMK	Removed virt_to_* and friends definitions
- */
-#ifndef __ASM_ARM_IO_H
-#define __ASM_ARM_IO_H
+#warning "You should include <sys/io.h>. This time I will do it for you."
+#include <sys/io.h>
 
-#endif	/* __ASM_ARM_IO_H */
