26a872effa31f941b47d4d0b8f11046a43167db0
mmazur <mmazur@11da6c6d-24c7-0310-97e0-b6d17e038e62>
2004-02-11 19:54:29 +0000

    - low level stuff... doesn't look userland to me

--- include/asm-ppc/time.h
+++
@@ -1,8 +0,0 @@
-/*
- * Common time prototypes and such for all ppc machines.
- *
- * Written by Cort Dougan (cort@fsmlabs.com) to merge
- * Paul Mackerras' version and mine for PReP and Pmac.
- */
-
-
