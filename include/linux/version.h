1dbca5094618c6debfec012b3ae77b135303c720
mmazur <mmazur@11da6c6d-24c7-0310-97e0-b6d17e038e62>
2004-05-02 22:26:50 +0000

    - releasing 2.6.5.1

--- include/linux/version.h
+++ include/linux/version.h
@@ -4,7 +4,7 @@
 
 #define UTS_RELEASE "2.6.5"
 #define LINUX_VERSION_CODE 132613
-#define LIBC_HEADERS_VERSION "2.6.5.0"
+#define LIBC_HEADERS_VERSION "2.6.5.1"
 #define KERNEL_VERSION(a,b,c) (((a) << 16) + ((b) << 8) + (c))
 
 #endif
