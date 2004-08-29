e09ea90fc0b7a9e23c48bdaa7c4a534ec95637cc
mmazur <mmazur@11da6c6d-24c7-0310-97e0-b6d17e038e62>
2004-08-29 13:04:50 +0000

    - releasing 2.6.8.1

--- linux-libc-headers/trunk/include/linux/version.h
+++ linux-libc-headers/trunk/include/linux/version.h
@@ -4,7 +4,7 @@
 
 #define UTS_RELEASE "2.6.8"
 #define LINUX_VERSION_CODE 132616
-#define LIBC_HEADERS_VERSION "2.6.8.0"
+#define LIBC_HEADERS_VERSION "2.6.8.1"
 #define KERNEL_VERSION(a,b,c) (((a) << 16) + ((b) << 8) + (c))
 
 #endif
