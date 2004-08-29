25e6938a1baf6fa92ebdb5d3660b6ce29d794297
qboosh <qboosh@11da6c6d-24c7-0310-97e0-b6d17e038e62>
2004-08-29 17:51:00 +0000

    - include <unistd.h> instead of providing own getpagesize() prototype - set PAGE_SHIFT to -1 instead of random trash if getpagesize() provides   value out of expected range

--- include/asm-ia64/page.h
+++ include/asm-ia64/page.h
@@ -10,11 +10,10 @@
 
 #include <asm/intrinsics.h>
 #include <asm/types.h>
+#include <unistd.h>
 
-extern int getpagesize(void);
 #define PAGE_SIZE ((unsigned long)getpagesize())
-#define PAGE_SHIFT ((unsigned long[]){12,13,14,-1,15,-1,-1,-1,16}[PAGE_SIZE>>13])
-
+#define PAGE_SHIFT ((PAGE_SIZE > 65536) ? -1 : ((unsigned long[]){12,13,14,-1,15,-1,-1,-1,16}[PAGE_SIZE>>13]))
 
 #define PAGE_MASK		(~(PAGE_SIZE - 1))
 #define PAGE_ALIGN(addr)	(((addr) + PAGE_SIZE - 1) & PAGE_MASK)
