9c705d45d108de57ed1e237d4541ce821f44af33
mmazur <mmazur@11da6c6d-24c7-0310-97e0-b6d17e038e62>
2004-10-21 16:04:54 +0000

    - removed __KERNEL__ stuff

--- linux-libc-headers/trunk/include/asm-m32r/param.h
+++ linux-libc-headers/trunk/include/asm-m32r/param.h
@@ -1,15 +1,8 @@
 #ifndef _ASM_M32R_PARAM_H
 #define _ASM_M32R_PARAM_H
 
-/* $Id: param.h,v 1.1 2004/10/21 15:15:37 mmazur Exp $ */
 
 /* orig : i386 2.5.67 */
-
-#ifdef __KERNEL__
-# define HZ		100		/* Internal kernel timer frequency */
-# define USER_HZ	100		/* .. some user interfaces are in "ticks" */
-# define CLOCKS_PER_SEC	(USER_HZ)	/* like times() */
-#endif
 
 #ifndef HZ
 #define HZ 100
