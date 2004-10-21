496302413f48fe4f1ef07fdde965c8c6afcd86cf
mmazur <mmazur@11da6c6d-24c7-0310-97e0-b6d17e038e62>
2004-10-21 15:15:40 +0000

    - added arch m32r (whatever that might be :)

---
+++ include/asm-m32r/siginfo.h
@@ -0,0 +1,8 @@
+#ifndef _M32R_SIGINFO_H
+#define _M32R_SIGINFO_H
+
+/* $Id: siginfo.h,v 1.1 2004/10/21 15:15:38 mmazur Exp $ */
+
+#include <asm-generic/siginfo.h>
+
+#endif /* _M32R_SIGINFO_H */
---
+++ include/asm-m32r/xor.h
@@ -0,0 +1,8 @@
+#ifndef _ASM_M32R_XOR_H
+#define _ASM_M32R_XOR_H
+
+/* $Id: xor.h,v 1.1 2004/10/21 15:15:39 mmazur Exp $ */
+
+#include <asm-generic/xor.h>
+
+#endif  /* _ASM_M32R_XOR_H */
