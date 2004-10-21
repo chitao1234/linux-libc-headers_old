e6a445ef38cf6ed9f3691f2b0d653ee1e4f0966d
mmazur <mmazur@11da6c6d-24c7-0310-97e0-b6d17e038e62>
2004-10-21 15:15:40 +0000

    - added arch m32r (whatever that might be :)

---
+++ linux-libc-headers/trunk/include/asm-m32r/errno.h
@@ -0,0 +1,9 @@
+#ifndef _ASM_M32R_ERRNO_H
+#define _ASM_M32R_ERRNO_H
+
+/* $Id: errno.h,v 1.1 2004/10/21 15:15:37 mmazur Exp $ */
+
+#include <asm-generic/errno.h>
+
+#endif /* _ASM_M32R_ERRNO_H */
+
