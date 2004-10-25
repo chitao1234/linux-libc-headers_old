4151febc02b66b788a59b26050ef8f7b85d8870f
mmazur <mmazur@11da6c6d-24c7-0310-97e0-b6d17e038e62>
2004-10-25 16:47:25 +0000

    - if using strict ansi don't define 64 bit types

--- linux-libc-headers/trunk/include/linux/types.h
+++ linux-libc-headers/trunk/include/linux/types.h
@@ -140,8 +140,11 @@ typedef __u16 __bitwise __le16;
 typedef __u16 __bitwise __be16;
 typedef __u32 __bitwise __le32;
 typedef __u32 __bitwise __be32;
+
+#if defined(__GNUC__) && !defined(__STRICT_ANSI__)
 typedef __u64 __bitwise __le64;
 typedef __u64 __bitwise __be64;
+#endif
 
 
 /*
