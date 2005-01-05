021ae4f0e34be726c50f41b7a1287438cfbc3ac6
mmazur <mmazur@11da6c6d-24c7-0310-97e0-b6d17e038e62>
2005-01-05 18:27:35 +0000

    - i386 goes ansi

--- scripts/verify.sh
+++ scripts/verify.sh
@@ -15,9 +15,15 @@ for arch in asm-alpha asm-arm asm-arm26 asm-cris asm-h8300 \
 	rm -f asm
 	ln -s $arch asm)
 
+	if [ "$arch" == "asm-i386" ]; then
+		CFLAGS="-ansi -pedantic -pedantic-errors"
+	else
+		CFLAGS=""
+	fi
+
 	for file in `cd ../../linux-libc-headers/include; find sound scsi asm linux -name '*.h'`; do
 		cat test.orig|sed -e "s,BLE,$file," >test.c
-		(gcc -I$LLHDIR/linux-libc-headers/include -Wall test.c) >/dev/null 2>&1
+		(gcc -I$LLHDIR/linux-libc-headers/include $CFLAGS test.c) >/dev/null 2>&1
 		if [ ! -f a.out ]; then
 			echo $arch:$file >>$OUT
 		fi
