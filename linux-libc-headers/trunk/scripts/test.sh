948c121759172bae102d5a50a445a2f9541751e5
mmazur <mmazur@11da6c6d-24c7-0310-97e0-b6d17e038e62>
2005-01-05 18:27:35 +0000

    - i386 goes ansi

--- linux-libc-headers/trunk/scripts/test.sh
+++ linux-libc-headers/trunk/scripts/test.sh
@@ -6,8 +6,14 @@ cd $LLHDIR/skrypty/.tmp
 rm -f asm
 ln -s $1 asm)
 
+if [ "$1" == "asm-i386" ]; then
+	CFLAGS="-ansi -pedantic -pedantic-errors"
+else
+	CFLAGS=""
+fi
+
 cat test.orig|sed -e "s,BLE,$2," >test.c
-gcc -I$LLHDIR/linux-libc-headers/include -Wall test.c
+gcc -I$LLHDIR/linux-libc-headers/include $CFLAGS -Wall test.c
 rm -f a.out test.c
 
 rm -f $LLHDIR/linux-libc-headers/include/asm
