f0b9dd77c1560d6384ed3d23ba2d7338453814ff
mmazur <mmazur@11da6c6d-24c7-0310-97e0-b6d17e038e62>
2005-03-26 18:56:16 +0000

    - releasing 2.6.11.2

--- linux-libc-headers/trunk/include/linux/version.h
+++ linux-libc-headers/trunk/include/linux/version.h
@@ -4,7 +4,7 @@
 
 #define UTS_RELEASE "2.6.11"
 #define LINUX_VERSION_CODE 132619
-#define LIBC_HEADERS_VERSION "2.6.11.1"
+#define LIBC_HEADERS_VERSION "2.6.11.2"
 #define KERNEL_VERSION(a,b,c) (((a) << 16) + ((b) << 8) + (c))
 
 #endif
