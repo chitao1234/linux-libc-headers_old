5174f67044adc4968b2bf49f39672101bdab7f42
mmazur <mmazur@11da6c6d-24c7-0310-97e0-b6d17e038e62>
2005-04-14 12:37:44 +0000

    - more Ubuntu/Debian patches (-ansi stuff)

--- linux-libc-headers/trunk/include/asm-sparc/types.h
+++ linux-libc-headers/trunk/include/asm-sparc/types.h
@@ -27,8 +27,10 @@ typedef unsigned short __u16;
 typedef __signed__ int __s32;
 typedef unsigned int __u32;
 
+#if defined(__GNUC__) && !defined(__STRICT_ANSI__)
 typedef __signed__ long long __s64;
 typedef unsigned long long __u64;
+#endif
 
 #endif /* __ASSEMBLY__ */
 
