a348add58e2d2e23abed3d5bcae3192272f167c5
pluto <pluto@11da6c6d-24c7-0310-97e0-b6d17e038e62>
2005-04-23 01:10:11 +0000

    - 2.6.12 will introduce at sparc64 new page sizes (64k, 512k, 4M).   [ http://linux.bkbits.net:8080/linux-2.5/patch%401.2181.2.53 ]   in fact we should calc shift page instead of lookup.
      moreover ultrasparc supports all page sizes from 8k up to 16GB
      in 8x steps but linux kernel doesn't support it all yet.

--- include/asm-sh64/page.h
+++ include/asm-sh64/page.h
@@ -19,8 +19,19 @@
 
 #include <unistd.h>
 
-#define PAGE_SIZE ((unsigned long)getpagesize())
-#define PAGE_SHIFT ((PAGE_SIZE > 65536) ? -1 : ((unsigned long[]){12,13,14,-1,15,-1,-1,-1,16}[PAGE_SIZE>>13]))
+#define PAGE_SIZE (getpagesize())
+static inline int getpageshift()
+{
+    int pagesize = getpagesize();
+#if (__GNUC__ >= 4) || ((__GNUC__ == 3) && (__GNUC_MINOR__ >= 4))
+    return (__builtin_clz(pagesize) ^ 31);
+#else
+    register int pageshift = -1;
+    while (pagesize) { pagesize >>= 1; pageshift++; }
+    return pageshift;
+#endif
+}
+#define PAGE_SHIFT (getpageshift())
 #define PAGE_MASK    (~(PAGE_SIZE-1))
 
 #endif /* __ASM_SH64_PAGE_H */
