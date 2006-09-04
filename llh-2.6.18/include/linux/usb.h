a93bc3668b715a6d96167bff1fdc86176b0839dc
cieciwa <cieciwa@11da6c6d-24c7-0310-97e0-b6d17e038e62>
2006-09-04 18:11:25 +0000

    - asm-i386 and linux from 2.6.18-rc6 [not cleaned!!]

---
+++ llh-2.6.18/include/linux/usb.h
@@ -0,0 +1,10 @@
+#ifndef __LINUX_USB_H
+#define __LINUX_USB_H
+
+#include <linux/mod_devicetable.h>
+#include <linux/usb_ch9.h>
+
+#define USB_MAJOR			180
+#define USB_DEVICE_MAJOR		189
+
+#endif
---
+++ llh-2.6.18/include/linux/usb_gadget.h
@@ -0,0 +1,18 @@
+/*
+ * <linux/usb_gadget.h>
+ *
+ * We call the USB code inside a Linux-based peripheral device a "gadget"
+ * driver, except for the hardware-specific bus glue.  One USB host can
+ * master many USB gadgets, but the gadgets are only slaved to one host.
+ *
+ *
+ * (C) Copyright 2002-2004 by David Brownell
+ * All Rights Reserved.
+ *
+ * This software is licensed under the GNU GPL version 2.
+ */
+
+#ifndef __LINUX_USB_GADGET_H
+#define __LINUX_USB_GADGET_H
+
+#endif	/* __LINUX_USB_GADGET_H */
