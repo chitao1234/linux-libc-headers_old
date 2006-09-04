a3d86cd3ca526c955bd3be8a9c3e098af9930e08
cieciwa <cieciwa@11da6c6d-24c7-0310-97e0-b6d17e038e62>
2006-09-04 17:41:56 +0000

    - asm-x86_64 from 2.6.18-rc6

---
+++ linux-libc-headers/trunk/llh-2.6.18/include/asm-x86_64/pci.h
@@ -0,0 +1,11 @@
+#ifndef __x8664_PCI_H
+#define __x8664_PCI_H
+
+#include <asm/io.h>
+
+/* generic pci stuff */
+#ifdef CONFIG_PCI
+#include <asm-generic/pci.h>
+#endif
+
+#endif /* __x8664_PCI_H */
