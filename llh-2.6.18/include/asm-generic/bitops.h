595b2bea68dd7e49ecf4915cd0ccc3a9156f01bc
pluto <pluto@11da6c6d-24c7-0310-97e0-b6d17e038e62>
2006-09-04 18:22:26 +0000

    userland shoud use gcc bitops builtins.

--- llh-2.6.18/include/asm-generic/bitops.h
+++ llh-2.6.18/include/asm-generic/bitops.h
@@ -1,20 +1,6 @@
 #ifndef _ASM_GENERIC_BITOPS_H_
 #define _ASM_GENERIC_BITOPS_H_
 
-/*
- * For the benefit of those who are trying to port Linux to another
- * architecture, here are some C-language equivalents.  You should
- * recode these in the native assembly language, if at all possible.
- * 
- * C language equivalents written by Theodore Ts'o, 9/26/92
- */
-
-#include <asm-generic/bitops/atomic.h>
-#include <asm-generic/bitops/non-atomic.h>
-#include <asm-generic/bitops/__ffs.h>
-#include <asm-generic/bitops/ffz.h>
-#include <asm-generic/bitops/fls.h>
-#include <asm-generic/bitops/fls64.h>
-#include <asm-generic/bitops/find.h>
+#error "use gcc builtins: http://gcc.gnu.org/onlinedocs/gcc-4.1.1/gcc/Other-Builtins.html#Other-Builtins"
 
 #endif /* _ASM_GENERIC_BITOPS_H */
